2cc9bd320eedcfab45dab49ef627414528ec6584
Felipe Beltramelo <60078289+beltramelofelipe@users.noreply.github.com>
2026-03-19 12:23:15 +0000

    into to agents

--- main.py
+++ main.py
@@ -6,30 +6,21 @@ from langchain.agents import create_agent
 from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_groq import ChatGroq
+from langchain_tavily import TavilySearch
 
 load_dotenv()
 
-def search(query: str) -> str:
-    """
-    Tool that searches over internet
-    Args:
-        query: The query to search for
-    Returns:
-        The search result    
-    """
-    print(f"Search for {query}")
-    return "Tokyo weather is sunny"
 
 llm = ChatGroq(
-        temperature=0, 
-        model_name="llama-3.3-70b-versatile",
+        temperature=0,
+        model_name="qwen/qwen3-32b",
         groq_api_key=os.getenv("GROQ_API_KEY"))
 
-tools = [search]
+tools = [TavilySearch()]
 agent = create_agent(model=llm, tools=tools)
 def main():
     print("Hello from langchain-course-f!")
-    result = agent.invoke({"messages": HumanMessage(content="What is the weather in Tokio?")})
+    result = agent.invoke({"messages": HumanMessage(content="search for 3 job postings for an ai engineer using langchain in the brazil area on linkedin and list their details?")})
     print(result)
 
 
